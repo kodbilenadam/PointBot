068f13d7e61a95d539bbcb408e249c10432af858
Miraç <natsu53100@gmail.com>
2017-06-29 04:54:38 +0300

    yanlış slash koymuşum :(

--- src/modules/commands/puan.rb
+++ src/modules/commands/puan.rb
@@ -4,12 +4,11 @@ module Bot
     module Puan
       extend Discordrb::Commands::CommandContainer
       command :puan do |event, *args|
-        File.open('/data/points.json', 'w+') do |pFile|
+        File.open('data/points.json', 'w+') do |pFile|
           points = JSON.parse(pFile)
           points[args[0]] += args[1].to_i
           pFile.write(points.to_json)
         end
-        # Puan komutları buraya gelecek
       end
     end
   end
