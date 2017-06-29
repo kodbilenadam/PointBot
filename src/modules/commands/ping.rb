aaa262d4b13199534bbe2a4bb78c9dd3dbcfa091
Miraç <natsu53100@gmail.com>
2017-06-29 06:32:23 +0300

    finish point system

--- src/modules/commands/ping.rb
+++ src/modules/commands/ping.rb
@@ -5,6 +5,7 @@ module Bot
     module Ping
       extend Discordrb::Commands::CommandContainer
       command :ping do |event|
+        break unless event.user.id == CONFIG.owner
         m = event.send 'Pong!'
         m.edit "Pong! `#{(Time.now - event.timestamp) * 1000} ms`"
       end
--- src/modules/commands/puan.rb
+++ src/modules/commands/puan.rb
@@ -4,6 +4,7 @@ module Bot
     module Puan
       extend Discordrb::Commands::CommandContainer
       command :puan do |event, *args|
+        break unless event.user.id == CONFIG.owner
         pFile = File.read('data/points.json')
         points = JSON.parse(pFile)
         points[args[0]] += args[1].to_i
@@ -13,10 +14,21 @@ module Bot
         points = JSON.parse(pFile)
         message = event.bot.servers[327885240775475201].channels[7].load_message(329808253678518284)
         text = "**Puanlar**\n"
+        values = Array.new
+        ids = [152562555376762881, 121825838856404995, 115464605676863492, 209085426768674816, 158288711799275521, 269403726735867904, 224196563939885066, 128096538231701504, 119825074038636548, 132788609320353792, 119822700809158656, 118825283963846656];
         points.each do |key, value|
-        text += "#{key}: **#{value}**\n"
+          values.push(value);
         end
-        message.edit(text);
+        names = Array.new
+        ids.each do |id|
+          names.push(event.bot.users[id].name)
+        end
+        counter = 0
+        while counter < names.length
+          text += "#{names[counter]}: **#{values[counter]}**\n"
+          counter += 1
+        end
+        message.edit(text)
         return
       end
     end
