46eb3fc9a9fcb0319c2b7126255fa64b0fdc461f
Miraç <natsu53100@gmail.com>
2017-06-29 05:46:12 +0300

    add point editing

--- src/modules/commands/puan.rb
+++ src/modules/commands/puan.rb
@@ -8,6 +8,11 @@ module Bot
         points = JSON.parse(pFile)
         points[args[0]] += args[1].to_i
         File.write('data/points.json', points.to_json)
+
+        pFile = File.read('data/points.json')
+        points = JSON.parse(pFile)
+        message = event.bot.servers[327885240775475201].channels[7].load_message(329808253678518284)
+        message.edit("")
         return
       end
     end
