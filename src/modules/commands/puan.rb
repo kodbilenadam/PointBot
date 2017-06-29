f3037c49c48e2688d74f122089152a9ea10e60ef
Miraç <natsu53100@gmail.com>
2017-06-29 04:37:34 +0300

    add point.rb

--- src/modules/commands/puan.rb
+++ src/modules/commands/puan.rb
@@ -3,7 +3,10 @@ module Bot
     # Puan ekleme bıdısı
     module Puan
       extend Discordrb::Commands::CommandContainer
-      command :puan do |event|
+      command :puan do |event, *args|
+        points = JSON.parse('.data/points.json')
+        points[args[0]] += args[1].to_i
+        points.to_json 
         # Puan komutları buraya gelecek
       end
     end
