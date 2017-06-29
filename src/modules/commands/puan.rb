530102f8e4734056401e7cc7bfed7aed68c79e15
Miraç <natsu53100@gmail.com>
2017-06-29 04:51:41 +0300

    another fix at point.rb

--- src/modules/commands/puan.rb
+++ src/modules/commands/puan.rb
@@ -4,10 +4,10 @@ module Bot
     module Puan
       extend Discordrb::Commands::CommandContainer
       command :puan do |event, *args|
-        pFile = File.read('.data/points.json')
-        points = JSON.parse(pfile)
-        points[args[0]] += args[1].to_i
-        points.to_json 
+        File.open('/data/points.json', 'w+') do |pFile|
+          points = JSON.parse(pFile)
+          points[args[0]] += args[1].to_i
+          pFile.write(points.to_json)
         # Puan komutları buraya gelecek
       end
     end
