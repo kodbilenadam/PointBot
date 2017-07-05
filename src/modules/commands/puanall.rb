c928436e66e76b679c3f071561504ce2243bafb6
Çağatay Karahan <kodbilenadam@gmail.com>
2017-07-06 01:19:04 +0300

    Update

--- src/modules/commands/puanall.rb
+++ src/modules/commands/puanall.rb
@@ -1,13 +1,15 @@
 module Bot
   module DiscordCommands
     # Puan ekleme bıdısı
-    module Puan
+    module PuanAll
       extend Discordrb::Commands::CommandContainer
-      command :puan do |event, *args|
+      command :puanall do |event, *args|
         break unless event.user.id == CONFIG.owner
         pFile = File.read('data/points.json')
         points = JSON.parse(pFile)
-        points[args[0]] += args[1].to_i
+        points.each do |key, value|
+          points[key] = value + args[0].to_i
+        end
         File.write('data/points.json', points.to_json)
 
         pFile = File.read('data/points.json')
@@ -15,7 +17,7 @@ module Bot
         message = event.bot.servers[327885240775475201].channels[7].load_message(329808253678518284)
         text = "**Puanlar**\n"
         values = Array.new
-        ids = [121825838856404995, 115464605676863492, 209085426768674816, 158288711799275521, 269403726735867904, 128096538231701504, 119825074038636548, 132788609320353792, 119822700809158656, 118825283963846656];
+        ids = [152562555376762881, 121825838856404995, 115464605676863492, 209085426768674816, 158288711799275521, 269403726735867904, 224196563939885066, 128096538231701504, 119825074038636548, 132788609320353792, 119822700809158656, 118825283963846656];
         points.each do |key, value|
           values.push(value);
         end
